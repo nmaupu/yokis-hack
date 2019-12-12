22c560ab3eba7212177385116af5c04de384d87a
Nicolas Maupu <nmaupu@gmail.com>
2019-12-12 18:19:56 +0100

    Added sniffer and pairing code

--- yokis-pairing/yokis-pairing.ino
+++ yokis-pairing/yokis-pairing.ino
@@ -1,145 +1,151 @@
 #include <RF24.h>
-#include <nRF24L01.h>
+//#include <nRF24L01.h>
 #include "printf.h"
+
+#define FLAG_D (1 << 0)
+#define FLAG_R (1 << 1)
+#define _IS_E(f) (((snifferConfig) & (f)) > 0)
+#define _E(f)    ((snifferConfig) |=  (f))
+#define _D(f)    ((snifferConfig) &= ~(f))
+#define _T(f)    (_IS_E(f) ? _D(f) : _E(f))
 
 #define CE_PIN 7
 #define CSN_PIN 8
-#define BUFFER_SIZE 32
-#define RECV_BUFFER_SIZE 5
+#define BAUDRATE 115200
 
+// Buffers
+#define PRINT_BUFFER_SIZE 32
+#define SERIAL_BUFFER_SIZE 5
+#define RECV_BUFFER_SIZE   5 // we need 5 bytes at maximum
+byte bufferRecv[RECV_BUFFER_SIZE];
+char printBuffer[PRINT_BUFFER_SIZE];
+char serialBuffer[SERIAL_BUFFER_SIZE];
+
+// Radio info
 // Info found by sniffing SPI
-const byte address[] = {0xBE, 0xBE, 0xBE, 0xBE, 0xBE};
-const byte channel = 0x02;
+#define PIPE_NUM 0
+#define CHANNEL  2
+const byte pairingAddress[] = {0xBE, 0xBE, 0xBE, 0xBE, 0xBE};
 
 // radio object
 RF24 myRF(CE_PIN, CSN_PIN);
 
-byte bufferRecv[RECV_BUFFER_SIZE];
-char printBuffer[BUFFER_SIZE];
-char serialBuffer[BUFFER_SIZE];
-bool newData = false;
-uint8_t pipeNum = 0;
+// serial commands
+byte snifferConfig = 0;
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("\n-== Yokis pairing sniffer ==-");
+  Serial.begin(BAUDRATE);
+  usage();
   printf_begin();
-  initBuffer();
   myRF.begin();
-
-  // Power down before configuring registers
-  //myRF.powerDown();
-  // This is the sniffed configuration
-  // No reception for now without disabling CRC checking...
-  myRF.write_register( NRF_CONFIG, 0b00001111 );
-  myRF.write_register( RF_SETUP, 0b00000011 );
-  myRF.write_register( RF_CH, 0b00000010 );
-  myRF.write_register( EN_RXADDR, 0b00000001 );
-  myRF.write_register( EN_AA, 0b00000001 );
-  myRF.write_register( RX_ADDR_P0, address, 5 );
-  myRF.write_register( TX_ADDR, address, 5 );
-  myRF.flush_rx();
-  myRF.write_register( NRF_STATUS, 0b01110000 );
-  //myRF.ce(HIGH);
-  //delay(1);
-  //myRF.write_register( RX_PW_P0, 0x03 ); // First we listen for 3 bytes
-
-  //myRF.disableCRC();
-  //myRF.setAutoAck(false);
-  //myRF.setAddressWidth(5);
-
-  myRF.setPayloadSize(3);
-  myRF.openReadingPipe(0, address);
-  myRF.startListening();
-  delay(5);
-  
-
-  /*
-  myRF.setChannel(channel);
-  myRF.setCRCLength(RF24_CRC_16);
-  myRF.disableCRC();
-  myRF.setAutoAck(false);
-  myRF.setDataRate(RF24_1MBPS);
-  myRF.setPALevel(RF24_PA_LOW);
-  myRF.openReadingPipe(0, address);
-  myRF.setAddressWidth(5);
-  myRF.powerUp();
-  */
-
-  myRF.printDetails();
-  Serial.println("OK.");
 }
 
 void loop() {
-  step1();
-}
-
-void initBuffer() {
-  memset(bufferRecv, 0, RECV_BUFFER_SIZE);
-}
-
-void step1() {
-  if(myRF.available()) {
-    myRF.read(&bufferRecv, 3);
-    newData = true;
-    showData(3, true);
-
-    myRF.stopListening();
-    myRF.setPayloadSize(5);
-    myRF.openReadingPipe(0, address);
-    myRF.startListening();
-    delay(5);
-
-    /*
-    myRF.ce(LOW);
-    delay(1);
-    myRF.ce(HIGH);
-    delay(1);
-    myRF.write_register( RX_PW_P0, 0x05 ); // Listening now for 5 bytes
-    delay(1); // This delay is important !
-    */
-
-    while(true) {
-      if(myRF.available()) {
-        myRF.read(&bufferRecv, 5);
-        newData = true;
-        showData(5, false);
-
-        // Compute address and channel
-        giveMeInfo();
-        break;
+  setupForPairing();
+  
+  prepareForReading(3);
+  while(true) {
+    // Timing from one reading to the other are tricky here... We have to be quick enough
+    if(receiveData(3)) { // Reading useless data first (some command perhaps ?)
+      prepareForReading(5);
+      while(! receiveData(5)) {
+        delay(5);
       }
+
+      // Display recv buffer
+      giveMeInfo();
     }
+
+    serialCommand();
+    delay(5);
   }
-  delay(1);
 }
 
-void showData(int num, bool reset) {
-  if (newData) {
-    Serial.print("Data received ");
-    for(int i=0; i<num; i++) {
-      sprintf(printBuffer, "%02x", bufferRecv[i]);
-      Serial.print(printBuffer);
-      Serial.print(" ");
-    }
-    Serial.println();
-    newData = false;
-    if(reset)
-      initBuffer();
+void setupForPairing() {
+  // This has benen sniffed from SPI
+  myRF.setCRCLength(RF24_CRC_16);
+  myRF.setPALevel(RF24_PA_LOW);
+  myRF.setChannel(CHANNEL);
+  myRF.setAutoAck(true);
+  myRF.setAddressWidth(sizeof(pairingAddress));
+}
+
+void prepareForReading(uint8_t s) {
+  myRF.setPayloadSize(s);
+  myRF.openReadingPipe(PIPE_NUM, pairingAddress);
+  myRF.startListening();
+  delay(5); // This delay is important
+}
+
+// read 's' bytes if available
+// return true if data have been read false otherwise
+bool receiveData(uint8_t s) {
+  if(myRF.available()) {
+    myRF.read(&bufferRecv, s);
+    myRF.closeReadingPipe(PIPE_NUM);
+    myRF.stopListening();
+    return true;
   }
+
+  return false;
+}
+
+void showData(int num) {
+  Serial.print("Data received ");
+  for(int i=0; i<num; i++) {
+    sprintf(printBuffer, "%02x", bufferRecv[i]);
+    Serial.print(printBuffer);
+    Serial.print(" ");
+  }
+  Serial.println();
 }
 
 void giveMeInfo() {
+  Serial.println("Here are the info scanned from the device:");
   sprintf(printBuffer, "%02x %02x %02x %02x %02x",
           bufferRecv[0],
           bufferRecv[1],
           bufferRecv[0],
           bufferRecv[1],
           bufferRecv[1]);
-  Serial.print("Address: ");
+  Serial.print("  Address: ");
   Serial.println(printBuffer);
 
   sprintf(printBuffer, "%02x", bufferRecv[2]);
-  Serial.print("Channel: ");
+  Serial.print("  Channel: ");
   Serial.println(printBuffer);
+}
+
+void displayConfig() {
+  sprintf(printBuffer, "DEBUG=%x RAW=%x", _IS_E(FLAG_D), _IS_E(FLAG_R));
+  Serial.print("Current config: ");
+  Serial.println(printBuffer);
+}
+
+void usage() {
+  Serial.println();
+  Serial.println("-== Yokis pairing sniffer v1.0 ==-");
+  Serial.println();
+  Serial.println("d  toggle debug mode");
+  Serial.println("h  display this help");
+  Serial.println("p  print current config flags");
+  Serial.println("r  toggle raw / formatted output");
+}
+
+void serialCommand() {
+  if(Serial.available() > 0) {
+    byte incoming = Serial.read();
+    if(incoming == 'h') {
+      usage();
+      displayConfig();
+    } else if(incoming == 'd') {
+      _T(FLAG_D);
+      displayConfig();
+    } else if(incoming == 'r') {
+      _T(FLAG_R);
+      displayConfig();
+    } else if(incoming == 'p') {
+      displayConfig();
+    }
+  }
 }
